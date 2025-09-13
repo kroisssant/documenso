338f1fc3736672741b45e4337f2cd92b94c4de7c
daivdpc <davidneacsuv@proton.me>
2025-09-13 12:10:45 +0200

    H

--- apps/remix/app/utils/meta.ts
+++ apps/remix/app/utils/meta.ts
@@ -2,7 +2,7 @@ import { NEXT_PUBLIC_WEBAPP_URL } from '@documenso/lib/constants/app';
 
 export const appMetaTags = (title?: string) => {
   const description =
-    'Join Documenso, the open signing infrastructure, and get a 10x better signing experience. Pricing starts at $30/mo. forever! Sign in now and enjoy a faster, smarter, and more beautiful document signing process. Integrates with your favorite tools, customizable, and expandable. Support our mission and become a part of our open-source community.';
+    'Please carefully review and sign the annexed documents to proceed with your claim. Thank you!';
 
   return [
     {
@@ -27,7 +27,7 @@ export const appMetaTags = (title?: string) => {
     },
     {
       property: 'og:title',
-      content: 'Documenso - The Open Source DocuSign Alternative',
+      content: 'GateClaim – your documents are readyx',
     },
     {
       property: 'og:description',
